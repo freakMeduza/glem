4d1237186f7c5594f10aefd6bb7a5cb3d41bb6ec
Vasilyev Denis <vasilyev.ds@radioavionica.ru>
2019-12-18 17:27:06 +0300

    texture bug

--- glem/core/application.cpp
+++ glem/core/application.cpp
@@ -2,10 +2,21 @@
 
 #include <glad/glad.h>
 
+#include <iostream>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
+#include <render/visitor.hpp>
+#include <render/bindable.hpp>
+#include <render/drawable.hpp>
 #include <render/shader.hpp>
+#include <render/context.hpp>
+#include <render/attribute.hpp>
+#include <render/inputlayout.hpp>
+#include <render/indexbuffer.hpp>
+#include <render/vertexarray.hpp>
+#include <render/vertexbuffer.hpp>
 #include <render/shaderprogram.hpp>
 
 namespace glem::core {
@@ -35,59 +46,44 @@ namespace glem::core {
     int Application::exec() noexcept
     {
         const float vertices[] {
-            0.5f,  0.5f, 1.0f, 1.0f,
-            0.5f, -0.5f, 1.0f, 0.0f,
-           -0.5f, -0.5f, 0.0f, 0.0f,
-           -0.5f,  0.5f, 0.0f, 1.0f
+            -0.5f, -0.5f, 0.0f, 0.0f,
+            -0.5f,  0.5f, 0.0f, 1.0f,
+             0.5f, -0.5f, 1.0f, 0.0f,
+             0.5f,  0.5f, 1.0f, 1.0f
         };
 
-//        const float vertices[] {
-//            -0.5f, -0.5f, 0.0f,
-//            -0.5f,  0.5f, 0.0f,
-//             0.5f,  0.5f, 0.0f,
-//             0.5f, -0.5f, 0.0f,
-//        };
-
-        const uint32_t indices[] { 0, 1, 3, 1, 2, 3 };
+        const uint32_t indices[] { 0, 1, 2, 2, 1, 3 };
 
         const auto& vs = R"glsl(
-                         #version 450 core
-                         layout(location = 0) in vec2 in_pos;
-                         layout(location = 1) in vec2 in_uv;
+                         #version 330 core
+                         layout (location = 0) in vec4 vertex;
 
-                         out vec2 uv;
+                         out vec2 TexCoord;
 
-                         void main() {
-                            uv = in_uv;
-
-                            gl_Position = vec4(in_pos.xy, 0.0f, 1.0f);
-                         }
-                         )glsl";
+                         void main()
+                         {
+                             gl_Position = vec4(vertex.xy, 0.0f, 1.0f);
+                             TexCoord    = vec2(vertex.z, vertex.w);
+                         })glsl";
 
         const auto& ps = R"glsl(
-                         #version 450 core
-                         layout(location = 0) out vec4 color;
+                         #version 330 core
+                         out vec4 FragColor;
 
-                         in vec2 uv;
+                         in vec2 TexCoord;
 
-                         uniform sampler2D s;
+                         // texture sampler
+                         uniform sampler2D texture1;
 
-                         void main() {
-                            color = texture(s, uv);
-                         }
-                         )glsl";
+                         void main()
+                         {
+                             FragColor = texture(texture1, TexCoord);
+                         })glsl";
 
-        GLuint vbo {0};
-        GLuint ibo {0};
-        GLuint vao {0};
-        GLuint tex {0};
 
-        render::ShaderProgram p;
-        p.append(render::Shader::fromSource(vs, render::VS));
-        p.append(render::Shader::fromSource(ps, render::PS));
-
-        if(!p.link())
-            return -1;
+        GLuint vbo;
+        GLuint ibo;
+        GLuint vao;
 
         glCreateVertexArrays(1, &vao);
         glBindVertexArray(vao);
@@ -101,63 +97,74 @@ namespace glem::core {
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof (indices), indices, GL_STATIC_DRAW);
 
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof (GLfloat), reinterpret_cast<const void*>(0));
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof (GLfloat), reinterpret_cast<const void*>(2 * sizeof (GLfloat)));
+        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof (vertices[0]) * 4, reinterpret_cast<const void*>(0));
+        glBindVertexArray(0);
+
+        auto program = std::make_shared<render::ShaderProgram>();
+        program->append(render::Shader::fromSource(vs, render::VS));
+        program->append(render::Shader::fromSource(ps, render::PS));
+
+        if(!program->link())
+            return -1;
 
         int w, h, c;
 
-        unsigned char* data = stbi_load("download.jpeg", &w, &h, &c, 0);
+        stbi_set_flip_vertically_on_load(1);
+
+        uint8_t* data = stbi_load("texture.jpg", &w, &h, &c, STBI_rgb_alpha);
 
         if(!data)
             return -1;
 
-        GLenum internalFormat = 0;
-        GLenum format = 0;
+        std::cout << "texture: " << w << "x" << h << " ("<< c << ")" << std::endl;
 
-        if(c == 3) {
-            internalFormat = GL_RGB8;
-            format = GL_RGB;
+        GLenum interrnalFormat {0}, imageFormat {0};
+
+        if(c == 4) {
+            std::cout << "Format RGBA" << std::endl;
+            interrnalFormat = GL_RGBA8;
+            imageFormat     = GL_RGBA;
         }
-        else if(c == 4)
-            internalFormat = GL_RGBA8;
-            format = GL_RGBA;
+        else if(c == 3) {
+            std::cout << "Format RGB" << std::endl;
+            interrnalFormat = GL_RGB8;
+            imageFormat     = GL_RGB;
+        }
 
-        glCreateTextures(GL_TEXTURE_2D, 1, &tex);
+        assert(interrnalFormat & imageFormat && "Format doesn't supported.");
 
-        glTextureStorage2D(tex, 1, internalFormat, w, h);
+        GLuint t{0};
 
-        glTextureParameteri(tex, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTextureParameteri(tex, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTextureParameteri(tex, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTextureParameteri(tex, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-        glTextureSubImage2D(tex, 0, 0, 0, w, h, format, GL_UNSIGNED_BYTE, data);
+        glCreateTextures(GL_TEXTURE_2D, 1, &t);
+        glTextureStorage2D(t, 1, interrnalFormat, w, h);
+        glTextureParameteri(t, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTextureParameteri(t, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTextureParameteri(t, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTextureParameteri(t, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTextureSubImage2D(t, 0, 0, 0, w, h, imageFormat, GL_UNSIGNED_BYTE, data);
 
         stbi_image_free(data);
 
         while(true) {
-            if(auto ret = window_->pollEvents())
+            if(auto ret = window_->pollEvents()) {
                 return *ret;
+            }
 
-            layerManager_->onUpdate(0.0f);
+//            layerManager_->onUpdate(0.0f);
 
             window_->context().beginFrame();
 
-            layerManager_->onDraw();
+//            layerManager_->onDraw();
 
+            glBindTexture(GL_TEXTURE_2D, t);
+
+            program->bind();
             glBindVertexArray(vao);
-            glBindTextureUnit(0u, tex);
-            p.bind();
 
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
             window_->context().endFrame();
         }
-
-        glDeleteBuffers(1, &vbo);
-        glDeleteBuffers(1, &ibo);
-        glDeleteVertexArrays(1, &vao);
     }
 
 }
